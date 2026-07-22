a14ef34c981013e0957f09cbf3254a9ad1cc0c5f
Mikhlik <mikhlik.dmitrii@gmail.com>
2026-07-21 17:23:01 -0700

    Add Windows OBS plugin build environment

---
+++ cmake/windows/buildspec.cmake
@@ -0,0 +1,21 @@
+# CMake Windows build dependencies module
+
+include_guard(GLOBAL)
+
+include(buildspec_common)
+
+function(_check_dependencies_windows)
+  set(arch ${CMAKE_VS_PLATFORM_NAME})
+  set(platform windows-${arch})
+
+  set(dependencies_dir "${CMAKE_CURRENT_SOURCE_DIR}/.deps")
+  set(prebuilt_filename "windows-deps-VERSION-ARCH-REVISION.zip")
+  set(prebuilt_destination "obs-deps-VERSION-ARCH")
+  set(obs-studio_filename "VERSION.zip")
+  set(obs-studio_destination "obs-studio-VERSION")
+  set(dependencies_list prebuilt obs-studio)
+
+  _check_dependencies()
+endfunction()
+
+_check_dependencies_windows()
